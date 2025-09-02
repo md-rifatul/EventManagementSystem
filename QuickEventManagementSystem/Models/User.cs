73fbc7fb686934a1230a9e4d2e2b43e2232ae550
Rifatul <rifatul.ism@gmail.com>
2025-09-02 22:48:27 +0600

    Add User Class

---
+++ QuickEventManagementSystem/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace QuickEventManagementSystem.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ContactNumber { get; set; }
+        public string Email { get; set; }
+        public UserType UserType { get; set; }
+    }
+}
