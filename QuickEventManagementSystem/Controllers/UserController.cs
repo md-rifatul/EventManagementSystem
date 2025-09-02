bf7e9a20674279c49036cbaed62efb45e822f978
Rifatul <rifatul.ism@gmail.com>
2025-09-02 23:55:53 +0600

    implement User, show all user

---
+++ QuickEventManagementSystem/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuickEventManagementSystem.Data;
+
+namespace QuickEventManagementSystem.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult AllUsers()
+        {
+            var users = SeedData.Users.ToList();
+            return View(users);
+        }
+    }
+}
