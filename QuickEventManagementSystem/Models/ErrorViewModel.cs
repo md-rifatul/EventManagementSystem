11faf44b75ecd4dbebbe0adc241ca99057c25d22
Rifatul <rifatul.ism@gmail.com>
2025-09-02 23:22:05 +0600

    Update

---
+++ QuickEventManagementSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace QuickEventManagementSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
