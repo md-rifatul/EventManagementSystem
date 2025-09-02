40c82f3fede79b5d7cc7e5cc224f6de162d9ac84
Rifatul <rifatul.ism@gmail.com>
2025-09-02 22:55:32 +0600

    Add Event class

---
+++ QuickEventManagementSystem/Models/Event.cs
@@ -0,0 +1,13 @@
+﻿namespace QuickEventManagementSystem.Models
+{
+    public class Event
+    {
+        public int EventId { get; set; }
+        public string EventName { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public string Address { get; set; }
+
+        public User AssignedUser { get; set; }
+    }
+}
