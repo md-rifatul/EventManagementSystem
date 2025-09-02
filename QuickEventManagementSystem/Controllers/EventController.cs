d4e1945293171f7fcc12634662dc2179f5bd5ba5
Rifatul <rifatul.ism@gmail.com>
2025-09-02 23:45:30 +0600

    implement all event

---
+++ QuickEventManagementSystem/Controllers/EventController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuickEventManagementSystem.Data;
+
+namespace QuickEventManagementSystem.Controllers
+{
+    public class EventController : Controller
+    {
+        public IActionResult AllEvents()
+        {
+            var events = SeedData.Events;
+            return View(events);
+        }
+    }
+}
